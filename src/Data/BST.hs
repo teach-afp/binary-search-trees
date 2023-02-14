d32542fa12940a2db7c92aa0f48ea7b6c0fca36f
Andreas Abel <andreas.abel@ifi.lmu.de>
2023-02-14 16:19:19 +0100

    BST library with basic cabal file for `cabal repl`
    A toy library for representing finite sets as (unbalanced) binary
    search trees (BSTs).
    
    Library provides methods for construction and destruction of BSTs.
    
    Essential cabal file, only defines library.
    - no test suite
    - no information for publishing on Hackage

---
+++ src/Data/BST.hs
@@ -0,0 +1,16 @@
+-- | Sets implemented as binary search trees, not necessarily balanced.
+
+module Data.BST (module Exports) where
+
+import Data.BST.Internal as Exports
+  ( BST
+  -- * Construction
+  , empty
+  , singleton
+  , insert
+  , fromList
+  , union
+  -- * Deletion
+  , delete
+  , split
+  )
---
+++ src/Data/BST/Internal.hs
@@ -0,0 +1,103 @@
+module Data.BST.Internal where
+
+-- | Binary search trees: ordered, not necessarily balanced trees.
+data BST a
+    -- | Empty search tree.
+  = Leaf
+    -- | Binary node in search tree.
+  | Node
+      (BST a)  -- ^ Left subtree, with elements ≤ label.
+      a        -- ^ Node label (pivot element).
+      (BST a)  -- ^ Right subtree, with element ≥ label.
+  deriving Show
+
+-- | Empty binary search tree.
+empty :: BST a
+empty = Leaf
+
+-- | Singleton binary search tree.
+singleton :: a -> BST a
+singleton a = Node Leaf a Leaf
+
+-- | Insert into binary search tree.
+insert :: Ord a
+  => a       -- ^ Element to be inserted.
+  -> BST a   -- ^ Tree to be inserted into.
+  -> BST a   -- ^ Tree enriched with the inserted element.
+insert a = go
+  where
+  go = \case
+    Leaf ->
+      singleton a
+    Node l p r ->
+      case compare a p of
+        LT -> Node (go l) p r
+        EQ -> Node l p r
+        GT -> Node l p (go r)
+
+-- | Create a binary search tree from a list.
+fromList :: Ord a
+  => [a]   -- ^ List to convert into tree.
+  -> BST a -- ^ Tree, not containing duplicates.
+fromList = foldr insert empty
+
+-- | Split a binary search tree along a new pivot element.
+split :: Ord a
+  => a              -- ^ Pivot element.
+  -> BST a          -- ^ Tree to be split along pivot element.
+  -> (BST a, BST a) -- ^ Left of pivot, right of pivot.
+                    --   Both not containing pivot element.
+split p = go
+  where
+  go = \case
+    Leaf ->
+      (Leaf, Leaf)
+    Node l a r ->
+      case compare p a of
+        LT -> (l', Node r' a r) where (l', r') = go l
+        EQ -> (l, r)
+        GT -> (Node l a l', r') where (l', r') = go r
+
+-- | Merge two binary search trees.
+union :: Ord a
+  => BST a  -- ^ Tree to be merged in.
+  -> BST a  -- ^ Tree to be merged into.
+  -> BST a  -- ^ Tree containing the union of elements.
+            --   Follows the structure of the second tree.
+union t = \case
+  Leaf ->
+    t
+  Node l p r ->
+    Node (union l' l) p (union r' r) where (l', r') = split p t
+
+-- | Delete from a binary search tree.
+delete :: Ord a
+  => a      -- ^ Element to be deleted.
+  -> BST a  -- ^ Tree to delete from.
+  -> BST a  -- ^ Tree containing everything but the deleted element.
+delete a = go
+  where
+  go = \case
+    Leaf ->
+      Leaf
+    Node l p r ->
+      case compare a p of
+        LT -> Node (go l) p r
+        EQ -> union l r
+        GT -> Node l p (go r)
+
+instance Ord a => Semigroup (BST a) where
+  (<>) = union
+
+instance Ord a => Monoid (BST a) where
+  mempty = empty
+
+instance Foldable BST where
+  foldMap :: Monoid m => (a -> m) -> BST a -> m
+  foldMap f = go
+    where
+    go = \case
+      Leaf ->
+        mempty
+      Node l p r ->
+        go l <> f p <> go r
